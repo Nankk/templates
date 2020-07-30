c590ff5a17d9073899994c766e680f5de04e8804
Nankk <toc32768@gmail.com>
2020-07-31 02:35:16 +0900

    :bookmark: Version 0.2.0 (my-electron)

--- my-electron/project.clj
+++ my-electron/project.clj
@@ -1,5 +1,5 @@
-(defproject my-electron/lein-template "0.1.0"
-  :description "FIXME: write description"
+(defproject my-electron/lein-template "0.2.0"
+  :description "Electron bootstrap template of mine"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
